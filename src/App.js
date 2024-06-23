da3f8cd0c6511b45c1bcd7fdf33a876cd695974d
Webmyn001 <bellomuhyideen0001@gmail.com>
2024-06-23 17:12:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router , Route, Routes, } from "react-router-dom";
-import Home from './Ileya/Client/Home'
-import Header from './Ileya/Client/Header';
+import Navbar from './Ileya/Admin/header/Navbar'
+import Admin from './Ileya/Admin/Admin';
+import Details from './Ileya/Admin/Details';
 
+import axios from 'axios'
 
 
 
 function App() {
 
-  
  
+const [loading, setloading] =useState(false)
+const [FormData, SetFormData] = useState([])
+
+
+
+    const getFormData = async  () => {
+        axios.get("https://ileya-backend.vercel.app/api/form/")
+        .then(res => {
+          console.log(res.data)
+          SetFormData(res.data)
+          setloading(true)
+        }).catch(err => {
+          console.log(err)
+        })
+       }
+
+      
+
+       useEffect(() => {
+        getFormData()
+       
+        }, [])
+
   return (
     <div className='font-josefins bg-[#ddd0c8]'>
-       
        <Router>
-    <Header/>
+            <Navbar/>
+       
          <Routes>
-               <Route path = "/" exact element= {<Home/>}></Route>
+               {/* Admin */}
+               <Route path = "/" exact element= {<Admin FormData={FormData} loading={loading}/>}></Route>
+               <Route path = "/details/:id" exact element= {<Details/>}></Route>
          </Routes>
          </Router> 
 
